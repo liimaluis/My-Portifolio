c9ff398ea8a10b8d908f6f65940afe75e5d5c01f
Luis Henrique <luish747474@gmail.com>
2025-10-10 17:42:16 -0300

    feat: Adicionando imagem e melhorando as sections de porjetos e sobre mim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,51 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 
 import { MatToolbar } from '@angular/material/toolbar';
 
+import {MatCardModule} from '@angular/material/card';
+
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatIcon],
+  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatIcon, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   
   title = 'portifolio';
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  ngAfterViewInit() {
+    this.typeEffect();
+  }
+
+  typeEffect() {
+    const element = document.getElementById('cargo');
+    if (!element) return;
+
+    const text = 'Desenvolvedor Full Stack';
+    let index = 0;
+
+    const typing = () => {
+      if (index < text.length) {
+        element.textContent = text.substring(0, index + 1);
+        index++;
+        setTimeout(typing, 100);
+      } else {
+        element.classList.add('typewriter');
+      }
+    };
+
+    typing();
+  }
+  
 }
