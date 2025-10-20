3e771d58157828788a5d3cdd79828eb72c837dcc
Luis Henrique Lima Gaiot <luis.lima@plural.com.br>
2025-10-20 17:58:02 -0300

    criação de componetes

---
+++ src/app/core/sections/conhecimentos/conhecimentos.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConhecimentosComponent } from './conhecimentos.component';
+
+describe('ConhecimentosComponent', () => {
+  let component: ConhecimentosComponent;
+  let fixture: ComponentFixture<ConhecimentosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConhecimentosComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ConhecimentosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/sections/conhecimentos/conhecimentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conhecimentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './conhecimentos.component.html',
+  styleUrl: './conhecimentos.component.css'
+})
+export class ConhecimentosComponent {
+
+}
