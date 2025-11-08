fa688ba0af0fde2bbdbc777a085abd502857324b
Luis Henrique Lima Gaiot <luis.lima@plural.com.br>
2025-11-08 11:43:10 -0300

    feat: Deixando Cards interativos

--- src/app/components/sections/cards/cards.component.ts
+++ src/app/components/sections/cards/cards.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MYPORTIFOLIO } from '../../portifolio.data';
 
 @Component({
   selector: 'app-cards',
   standalone: true,
   imports: [MatCardModule],
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.css'
+  styleUrls: ['./cards.component.css'] // ✅ plural e array
 })
 export class CardsComponent {
-
+  projetos = MYPORTIFOLIO;
 }
