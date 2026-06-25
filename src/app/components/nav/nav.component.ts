927ae041f0888b0caa0d32ec29571799b460724a
Luis Henrique <luisg.tecnologia@clubepaineiras.com.br>
2026-06-24 21:30:00 -0300

    refatorando navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,16 +2,24 @@ import { Component, ViewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterOutlet, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatIcon],
+  imports: [
+    RouterOutlet, 
+    MatSidenav, 
+    MatSidenavContainer, 
+    MatSidenavContent, 
+    MatToolbar, 
+    MatIcon,
+    MatButtonModule
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
-    @ViewChild('sidenav') sidenav!: MatSidenav;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 }
